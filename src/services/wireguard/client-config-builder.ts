3dc9d18285a73d4282b891d927fa67d7fa93935f
Daniel Mesa <dmesa1994@gmail.com>
2025-08-03 11:22:07 -0400

    fix: update iptables rules in client config to ensure correct interface handling

--- src/services/wireguard/client-config-builder.ts
+++ src/services/wireguard/client-config-builder.ts
@@ -16,8 +16,8 @@ Address = ${node.address}/32
 ${node.dns ? `DNS = ${node.dns}\n` : ''}
 ${
   node.isGateway
-    ? `${node.gatewayNetworks.map((net) => `PostUp = iptables -A FORWARD -i ${node.wgInterface} -o ${net.interface} -s ${serverConfig.subnet} -d ${net.subnet} -j ACCEPT; iptables -A FORWARD -i ${net.interface} -o ${node.wgInterface} -s ${serverConfig.subnet} -d ${net.subnet} -j ACCEPT; iptables -t nat -A POSTROUTING -s ${serverConfig.subnet} -d ${net.subnet} -o ${net.interface} -j MASQUERADE`).join(`\n`)}
-${node.gatewayNetworks.map((net) => `PostDown = iptables -D FORWARD -i ${node.wgInterface} -o ${net.interface} -s ${serverConfig.subnet} -d ${net.subnet} -j ACCEPT; iptables -D FORWARD -i ${net.interface} -o ${node.wgInterface} -s ${serverConfig.subnet} -d ${net.subnet} -j ACCEPT; iptables -t nat -D POSTROUTING -s ${serverConfig.subnet} -d ${net.subnet} -o ${net.interface} -j MASQUERADE`).join(`\n`)}`
+    ? `${node.gatewayNetworks.map((net) => `PostUp = iptables -A FORWARD -i ${node.wgInterface} -o ${net.interface} -s ${serverConfig.subnet} -d ${net.subnet} -j ACCEPT; iptables -A FORWARD -i ${net.interface} -o ${node.wgInterface} -s ${net.subnet} -d ${serverConfig.subnet} -j ACCEPT; iptables -t nat -A POSTROUTING -s ${serverConfig.subnet} -d ${net.subnet} -o ${net.interface} -j MASQUERADE`).join(`\n`)}
+${node.gatewayNetworks.map((net) => `PostDown = iptables -D FORWARD -i ${node.wgInterface} -o ${net.interface} -s ${serverConfig.subnet} -d ${net.subnet} -j ACCEPT; iptables -D FORWARD -i ${net.interface} -o ${node.wgInterface} -s ${net.subnet} -d ${serverConfig.subnet} -j ACCEPT; iptables -t nat -D POSTROUTING -s ${serverConfig.subnet} -d ${net.subnet} -o ${net.interface} -j MASQUERADE`).join(`\n`)}`
     : ''
 }
 
