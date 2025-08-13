a3505ebf14f7c68bc7416697332d90143e320d8c
Daniel Mesa <dmesa1994@gmail.com>
2025-08-13 12:25:52 -0400

    chore: remove defaults certs and generate them in init script

--- docker/resources/init.sh
+++ docker/resources/init.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+mkdir -p /data/ssl
+
+openssl genpkey -algorithm RSA -out /data/ssl/privkey.key
+openssl req -new -key /data/ssl/privkey.key -out /data/ssl/default.csr -config /etc/openssl/openssl.cnf
+openssl x509 -req -days 3650 -in /data/ssl/default.csr -signkey /data/ssl/privkey.key -out /data/ssl/cert.crt
+
+ln -sfn /data/ssl/privkey.key /etc/nginx/ssl/privkey.key
+ln -sfn /data/ssl/cert.crt /etc/nginx/ssl/cert.crt
+
 env | while IFS='=' read -r key value; do
   printf '%s=%q\n' "$key" "$value"
 done > /etc/environment
